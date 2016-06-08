99bbb2f0799a40c0218dd20e23770450cab4679d
Destin Hebner <destinhebner@gmail.com>
2016-06-08 13:15:56 -0400

    finished up migrations

--- oovent-migrations/Migration06062016.cs
+++ oovent-migrations/Migration06062016.cs
@@ -16,14 +16,14 @@ namespace Oovent.Migrations
             Delete.Table("entity_urls");
             Delete.Table("urls");
             Delete.Table("url_types");
+            Delete.Table("entity_user_info_entity_relationships");
             Delete.Table("entity_user_info");
             Delete.Table("entity_tags");
-            Delete.Table("entities");
+            Delete.Table("entities");            
             Delete.Table("entity_type_allowed_child_types");
             Delete.Table("entity_types");
             Delete.Table("tags");
         }
-
 
         public override void Up()
         {
@@ -55,9 +55,13 @@ namespace Oovent.Migrations
 
             Create.Table("entity_user_info")
                 .WithColumn("id").AsInt32().NotNullable().Identity().PrimaryKey()
-                .WithColumn("entity_id").AsInt32().Nullable().ForeignKey("entities", "id")
                 .WithColumn("email").AsString().NotNullable()
-                .WithColumn("password").AsString().NotNullable().GetHashCode();
+                .WithColumn("password").AsString().NotNullable();
+
+            Create.Table("entity_user_info_entity_relationships")
+                .WithColumn("id").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("entity_id").AsInt32().ForeignKey("entities", "id")
+                .WithColumn("entity_user_info").AsInt32().ForeignKey("entity_user_info", "id");
 
             Create.Table("url_types")
                 .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
@@ -143,7 +147,5 @@ namespace Oovent.Migrations
                 .Row(new { id = "6", name = "Created By" })
                 .Row(new { id = "7", name = "Located At" });            
         }
-
-
     }
 }
