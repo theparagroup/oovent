f7db6a9d64ba38126973390134ecca10828b6a1c
Destin Hebner <destinhebner@gmail.com>
2016-08-02 16:49:49 -0400

    reorganized public controllers

--- oovent-mvc/Areas/Public/Controllers/LandingController.cs
+++ oovent-mvc/Areas/Public/Controllers/LandingController.cs
@@ -3,81 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Oovent.Mvc.Areas.Public.Models;
-using Oovent.Mvc.Areas.Public;
-using com.paralib.Utils;
-using Oovent.Models.Ef;
-using com.paralib.Mvc.Authorization;
-using com.paralib.Mvc.Authentication;
-using Oovent.Models;
 
 namespace Oovent.Mvc.Areas.Public.Controllers
 {
-    [Route("~/{action}")]
-    public class LandingController : PublicController
+    public class LandingController : Controller
     {
+        // GET: Public/Landing
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult Unauthorized()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Login()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                return Redirect("~/");
-            }
-            else
-            {
-                return View();
-            }
-        }
-
-        [HttpPost]
-        public ActionResult Login(VmLogin vmLogin)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = EntityUserInfo.GetUser(vmLogin.Email);
-                List<string> roles = new List<string>();
-
-                if (user == null)
-                {
-                    ModelState.AddModelError("", "That email was not found on our records.");
-                }
-                else
-                {
-                    if (Crypto.VerifyPassword(user.Password, vmLogin.Password))
-                    {
-                        using (DbContext db = new DbContext())
-                        {
-                            var query = from e in db.Entities
-                                        where e.Id == user.EntityId && e.EntityTypeId == 2
-                                        select e;
-
-                            if (query.Count() > 0)
-                            {
-                                roles.Add("admin");
-                            }
-                        }
-
-                        ParaPrinciple paraPrinciple = new ParaPrinciple(user.Email, roles.ToArray());
-                        Forms.Authenticate(paraPrinciple);
-                        return Redirect("~/admin/dashboard");
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("", "That password was incorrect.");
-                    }
-                }
-            }
-            return View(vmLogin);
         }
     }
 }
---
+++ oovent-mvc/Areas/Public/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Oovent.Models;
+using Oovent.Models.Ef;
+using com.paralib.Utils;
+using com.paralib.Mvc.Authorization;
+using Oovent.Mvc.Areas.Public.Models;
+using com.paralib.Mvc.Authentication;
+
+namespace Oovent.Mvc.Areas.Public.Controllers
+{
+    public class LoginController : PublicController
+    {
+        [Route("~/login")]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Redirect("~/");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [Route("~/login")]
+        [HttpPost]
+        public ActionResult Login(VmLogin vmLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = EntityUserInfo.GetUser(vmLogin.Email);
+                List<string> roles = new List<string>();
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "That email was not found on our records.");
+                }
+                else
+                {
+                    if (Crypto.VerifyPassword(user.Password, vmLogin.Password))
+                    {
+                        using (DbContext db = new DbContext())
+                        {
+                            var query = from e in db.Entities
+                                        where e.Id == user.EntityId && e.EntityTypeId == 2
+                                        select e;
+
+                            if (query.Count() > 0)
+                            {
+                                roles.Add("admin");
+                            }
+                        }
+
+                        ParaPrinciple paraPrinciple = new ParaPrinciple(user.Email, roles.ToArray());
+                        Forms.Authenticate(paraPrinciple);
+                        return Redirect("~/admin/dashboard");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "That password was incorrect.");
+                    }
+                }
+            }
+            return View(vmLogin);
+        }
+    }
+}
