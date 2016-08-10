c608b15a762f834b47fcd66df5ad8dbf30e98d63
Destin Hebner <destinhebner@gmail.com>
2016-08-10 15:07:14 -0400

    working on index page, updated test data

--- oovent-mvc/Areas/Public/Controllers/LandingController.cs
+++ oovent-mvc/Areas/Public/Controllers/LandingController.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Oovent.Mvc.Areas.Public.Models.Events;
 
 namespace Oovent.Mvc.Areas.Public.Controllers
 {
     public class LandingController : PublicController
     {
         [Route("~/Index")]
-        // GET: Public/Landing
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("~/LoadEvents")]
+        public string LoadEvents()
+        {
+            return "Test";
+        }
+
         [Route("~/Unauthorized")]
-        // GET: Public/Landing
         public ActionResult Unauthorized()
         {
             return View();
--- oovent-mvc/Static/Libraries/frontend/index-filter.js
+++ oovent-mvc/Static/Libraries/frontend/index-filter.js
@@ -1,6 +1,43 @@
 ﻿
-$("#filter-type-happening").click(function ()
-{
-    alert("Test");
-    $("#event-type-1").hide("slow");
+$(document).ready(function ()
+{ 
+    $(".filter-type").data("data-type", "happening").click(function()
+    {
+        alert("CLICKED");
+        $.ajax({
+            type: 'GET',
+            url: "LoadEvents",
+            data: data,
+            async: false,
+            beforeSend: function (xhr) {
+                if (xhr && xhr.overrideMimeType) {
+                    xhr.overrideMimeType('application/json;charset=utf-8');
+                }
+            },
+            dataType: 'json',
+            success: function (data) {
+                alert("Ajaxed");
+            }
+        });
+    });
+
+        //if ($(".filter-type").data("data-type", "happening"))
+        //{
+        //    if ($(".filter-type").data("data-type", "happening").is(":checked"))
+        //    {
+        //        $(".event-type-1").css("display", "inline");
+        //    }
+        //    else
+        //    {
+        //        $(".event-type-1").css("display", "none");
+        //    }
+        //}
+        //else if ($(".filter-type").data("data-type", "discount"))
+        //{
+        //    $(".event-type-2").toggle(300);
+        //}
+        //else if ($(".filter-type").data("data-type", "foodtruck"))
+        //{
+        //    $(".event-type-3").toggle(300);
+        //}
 });
